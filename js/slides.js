df5a03b42a636bc2e91f6e167af50186debf5908
Szymon Nowak <szimek@gmail.com>
2014-02-05 15:13:16 +0100

    Really initial commit

---
+++ js/slides.js
@@ -0,0 +1,13 @@
+require([
+    'order!../slide_config',
+    'order!modernizr.custom.45394',
+    'order!prettify/prettify',
+    'order!hammer',
+    'order!firebase/firebase',
+    'order!firebase/firebase-simple-login',
+    'order!firebase/firebase-sync',
+    'order!slide-controller',
+    'order!slide-deck'
+], function (module) {
+
+});
